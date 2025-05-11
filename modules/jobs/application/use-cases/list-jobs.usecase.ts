0cd1c7cbb6b2bc417e896f65b278d1497cdb2d1c
khalil meziane <khalilmez2000@gmail.com>
2025-05-11 22:47:21 +0100

    fix: type issue

--- modules/jobs/application/use-cases/list-jobs.usecase.ts
+++ modules/jobs/application/use-cases/list-jobs.usecase.ts
@@ -1,14 +1,14 @@
-import { IJobApplication } from "../../domain/entities/job.entity"
-import { GetJobsParamsDTO } from "../dtos/GetJobsParamsDTO"
-import { JobService } from "../services/job.service"
+import { IJobApplication } from "../../domain/entities/job.entity";
+import { GetJobsParamsDTO } from "../dtos/GetJobsParamsDTO";
+import { JobService } from "../services/job.service";
 
 export class ListJobsUseCase {
-  constructor(private readonly jobService: JobService) {}
+  constructor(private readonly jobService: JobService) { }
 
   async execute(
     userId: number,
     params: GetJobsParamsDTO
-  ): Promise<IJobApplication[]> {
+  ): Promise<{ jobs: IJobApplication[]; totalCount: number }> {
     return this.jobService.listJobsForUser(userId, params)
   }
 }
